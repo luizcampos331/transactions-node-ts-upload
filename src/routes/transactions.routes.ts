84c37094f1c0b425d68bb4f2f8e3f6e9d04e6b90
luizcampos331 <luizcampos331@gmail.com>
2020-09-17 11:45:23 -0300

    add Axios

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { getCustomRepository, getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import uploadConfig from '../config/upload';
-
-import Category from '../models/Category';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
@@ -15,29 +13,9 @@ const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
-  const transactionsTemp = await transactionsRepository.find();
+
+  const transactions = await transactionsRepository.find();
   const balance = await transactionsRepository.getBalance();
-
-  const categoryRepository = getRepository(Category);
-  const categoryPromise = transactionsTemp.map(transaction =>
-    categoryRepository.findOne({ id: transaction.category_id }),
-  );
-
-  const category = await Promise.all(categoryPromise);
-
-  const transactions: object[] = [];
-
-  transactionsTemp.forEach(async (transaction, index) => {
-    transactions.push({
-      id: transaction.id,
-      tile: transaction.title,
-      value: transaction.value,
-      type: transaction.type,
-      category: category[index],
-      created_at: transaction.created_at,
-      updated_at: transaction.updated_at,
-    });
-  });
 
   const transactionsAndBalance = {
     transactions,
